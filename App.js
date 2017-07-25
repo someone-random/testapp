47edcf26093d5525ca564922f97bb8c8411d42aa
Admin <admin@Admins-MacBook-Pro-3.local>
2017-07-25 21:26:12 +0700

    array value for red changed according to button presses

--- App.js
+++ App.js
@@ -3,32 +3,55 @@ import { AppRegistry, Text, View, StyleSheet,TouchableHighlight,Alert } from 're
 
 
 export default class HelloWorldApp extends Component {
-  render() {
+  constructor(props){
+    super(props);
+    this.state = ({
+      a:[3],
+      b:[3],
+      red: []
+    })
+    this.handleClick=this.handleClick.bind(this)
+    console.log(this.state.red)
+    for (var i=0;i<3;i++){
+      this.state.a[i]=i+1
+      this.state.b[i]=i+4
+      this.state.red[i]=false
+    }
+  }
+  handleClick(key){
+    console.log(key)
+    if(this.state.red[key-4] === true){
+      this.state.red[key-4] = false
+    } else {
+      this.state.red[key-4] = true
+    }
+    console.log(this.state.red[0])
+    console.log(this.state.red[1])
+    console.log(this.state.red[2])
+  }
+  render(){
+    console.log(this.state.red)
     return (
       <View style={styles.bigBox}>
         <View style={styles.box}>
-          <TouchableHighlight style={styles.button} onPress={() => {Alert.alert('Hello world')}}>
+        {this.state.a.map((number) =>(
+          <TouchableHighlight style={styles.button} onPress={() => {Alert.alert('Hello world')}} key={number}>
             <View>
-              <Text style={styles.text}>1</Text>
+              <Text style={styles.text}>{number}</Text>
             </View>
           </TouchableHighlight>
-          <View style={styles.button}>
-            <Text style={styles.text}>2</Text>
-          </View>
-          <View style={styles.button}>
-            <Text style={styles.text}>3</Text>
-          </View>
+        )
+      )}
         </View>
         <View style={styles.box}>
-          <View style={styles.button}>
-            <Text style={styles.text}>4</Text>
-          </View>
-          <View style={styles.button}>
-            <Text style={styles.text}>5</Text>
-          </View>
-          <View style={styles.button}>
-            <Text style={styles.text}>6</Text>
-          </View>
+        {this.state.b.map((number) => (
+          <TouchableHighlight style={styles.button} onPress={() => {this.handleClick(number)}} key={number}>
+            <View>
+              <Text style={styles.text}>{number}</Text>
+              {console.log(number)}
+            </View>
+          </TouchableHighlight>
+        ))}
         </View>
         <View style={styles.box}>
           <View style={styles.button}>
@@ -71,4 +94,14 @@ const styles = StyleSheet.create({
     paddingTop:50,
     paddingBottom:50
   },
+  red:{
+    backgroundColor: 'red',
+    flex: 1,
+    alignItems: 'center',
+    margin:10,
+    shadowColor:'red',
+    shadowRadius: 20,
+    shadowOpacity: 1,
+    shadowOffset: { width: 10, height: 5 },
+  }
 })
